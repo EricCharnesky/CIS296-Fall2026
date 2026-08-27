79c3f105b65aad6612c5764fedeb461dd308a21d
Eric Charnesky <2469199+EricCharnesky@users.noreply.github.com>
2026-08-27 19:46:39 -0400

    hello classmate

--- Week0-HelloWorld/src/Main.java
+++ Week0-HelloWorld/src/Main.java
@@ -2,6 +2,9 @@
 
 void main() {
 
-    IO.println("Hello world!");
+    IO.println("Eric Charnesky he/him Doctoral Dropout");
+    System.out.println("Teach at OCC, 7 kids, play D&D, run marathons");
+    System.out.println("ABD");
+    System.out.println("Population health management and analytics");
 
 }
