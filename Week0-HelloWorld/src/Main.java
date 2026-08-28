f63c26e32ac9d4bfdf49fe16e792e27fb820e1ad
Eric Charnesky <2469199+EricCharnesky@users.noreply.github.com>
2026-08-27 20:06:16 -0400

    some variables and class

--- Week0-HelloWorld/src/Main.java
+++ Week0-HelloWorld/src/Main.java
@@ -1,10 +1,15 @@
 
+public class Main {
 
-void main() {
+    public static void main() {
+        int favoriteNumber = 42;
+        
+        String name = "Eric Charnesky";
 
-    IO.println("Eric Charnesky he/him Doctoral Dropout");
-    System.out.println("Teach at OCC, 7 kids, play D&D, run marathons");
-    System.out.println("ABD");
-    System.out.println("Population health management and analytics");
+        IO.println(name + " he/him Doctoral Dropout");
+        System.out.println("Teach at OCC, 7 kids, play D&D, run marathons");
+        System.out.println("ABD");
+        System.out.println("Population health management and analytics");
 
+    }
 }
